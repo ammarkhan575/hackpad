6a02b6311c74f27b5527655ece9ab6571e31c54e
ammarkhan575 <ammarkhan575@gmail.com>
2026-02-22 16:22:02 +0530

    ADD: update go.mod and go.sum with new dependencies; refactor main.go for Bubble Tea integration and improve makefile

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	msg string
+}
+
+// Init initializes the model and returns an initial command.
+// In this case, we don't have any initial commands to run, so we return nil.
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	 switch msg := msg.(type) {
+
+    // Is it a key press?
+    case tea.KeyMsg:
+
+        // Cool, what was the actual key pressed?
+        switch msg.String() {
+
+        // These keys should exit the program.
+        case "ctrl+c", "q":
+            return m, tea.Quit
+
+    }
+
+    // Return the updated model to the Bubble Tea runtime for processing.
+    // Note that we're not returning a command.
+    return m, nil
+}
+
+func (m model) View() string {
+	return m.msg
+}
+
+func initialModel() model {
+	return model{
+		msg: "Hello, Welcome to Hackpad",
+	}
+}
 
 func main() {
-	fmt.Println("Hello, Welcome to Hackpad")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 build:
-	@go build -o hackpad .
-
-run:
-	@./hackpad
+	go build -o hackpad .
+# @ is used to prevent the command from being printed in the terminal
+# when we run make run, it will first build the project and then execute the binary
+run: build
+	@./hackpad 
